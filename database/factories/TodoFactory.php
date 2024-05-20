4a10a9e95ee6936ff6d04603f37b966bb05abfd5
htetoozin <htetoo.zin09@gmail.com>
2024-05-20 10:19:07 +0700

    create todo factory

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    protected $model = Todo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'is_active' => $this->faker->randomElement([0,1]),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
